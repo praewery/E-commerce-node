08fae850678cdba9481ba36d8f79c726ce0e30c0
Praewa Choobanna <cytokinin8@gmail.com>
2025-01-19 21:04:40 +0700

    create register

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const prisma = require('../config/prisma')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 exports.register = async(req, res) => {
     //code
     try{
@@ -11,10 +15,30 @@ exports.register = async(req, res) => {
         }
 
         //step2 db already?
+        const user = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+        if(user){
+            return res.status(400).json({ message: "Email aready Exits" })
+        }
 
+        //step3 hash password
+        const hashPassword = await bcrypt.hash(password,10) //เกลือคือเลขที่ปนตอนใส่รหัสเลขแต่รหัสเลขจะไม่เหมือนกัน
+        
 
-        console.log(email, password)
-        res.send('hello Register controller')
+        //step4 register
+        await prisma.user.create({
+            data: {
+                email: email,
+                password: hashPassword
+            }
+        })
+
+        
+
+        res.send('register success')
 
     }catch(err){
         console.log(err)
@@ -24,6 +48,38 @@ exports.register = async(req, res) => {
 
 exports.login = async(req, res) => {
     try{
+        const { email, password } = req.body
+        //step1 check email
+        const user = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+        if(!user){
+            return res.status(400).json({ message: "Email not found" })
+        }
+        //step2 check password  userเอาจากตัวที่เช็คด้านบน
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){ 
+            return res.status(400).json({ message: "Password not match" })
+          }
+        //step3 create payload เอาไว้เข้ารหัสtoken
+        const payload = {
+            id : user.id,
+            email: user.email,
+            role: user.role
+        }
+        
+        //step4 create token
+        jwt.sign(payload,process.env.JWT_SECRET,{ expiresIn: '1d' },(err,token)=>{
+            if(err) {
+                return res.status(500).json({ message: "Server Error" })
+
+            }
+            res.json({ token })
+        }) 
+        console.log(payload)
+
         res.send('hello login controller')
 
     }catch(err){
