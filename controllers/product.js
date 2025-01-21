5460ef181877d67186dd57aa55a4f54e980f9508
Praewa Choobanna <cytokinin8@gmail.com>
2025-01-21 19:41:11 +0700

    Update product(func remove,listby)

--- controllers/product.js
+++ controllers/product.js
@@ -1,8 +1,9 @@
-const prisma = require('../config/prisma')
+const prisma = require("../config/prisma");
 
 exports.create = async (req, res) => {
   try {
-    const { title, description, price, quantity, categoryId, images } = req.body;
+    const { title, description, price, quantity, categoryId, images } =
+      req.body;
     const product = await prisma.product.create({
       data: {
         title: title,
@@ -10,13 +11,13 @@ exports.create = async (req, res) => {
         price: parseFloat(price),
         quantity: parseInt(quantity),
         categoryId: parseInt(categoryId),
-          images: {
-            create: images.map((item) => ({
-                asset_id: item.asset_id,
-                public_id: item.public_id,
-                url: item.url,
-                secure_url: item.secure_url
-            }))
+        images: {
+          create: images.map((item) => ({
+            asset_id: item.asset_id,
+            public_id: item.public_id,
+            url: item.url,
+            secure_url: item.secure_url,
+          })),
         },
       },
     });
@@ -29,17 +30,34 @@ exports.create = async (req, res) => {
 };
 exports.list = async (req, res) => {
   try {
-    const{count}  = req.params
+    const { count } = req.params;
     const products = await prisma.product.findMany({
-        take: parseInt(count),
-        orderBy: { createdAt : 'desc' },//desc = สินค้าเรียงจากมากไปน้อย
-        include: {
-            category: true,
-            images: true,
-            
-        }
-    })
-    console.log(count)
+      take: parseInt(count),
+      orderBy: { createdAt: "desc" }, //desc = สินค้าเรียงจากมากไปน้อย
+      include: {
+        category: true,
+        images: true,
+      },
+    });
+    console.log(count);
+    res.send(products);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+exports.read = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await prisma.product.findMany({
+      where: {
+        id: Number(id),
+      },
+      include: {
+        category: true,
+        images: true,
+      },
+    });
     res.send(products);
   } catch (err) {
     console.log(err);
@@ -48,6 +66,36 @@ exports.list = async (req, res) => {
 };
 exports.update = async (req, res) => {
   try {
+    const { title, description, price, quantity, categoryId, images } =
+      req.body;
+    //clear รูปเก่า
+    await prisma.image.deleteMany({
+      where: {
+        productId: parseInt(req.params.id),
+      },
+    });
+    //update ข้อมูล
+    const product = await prisma.product.update({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      data: {
+        title: title,
+        description: description,
+        price: parseFloat(price),
+        quantity: parseInt(quantity),
+        categoryId: parseInt(categoryId),
+        images: {
+          create: images.map((item) => ({
+            asset_id: item.asset_id,
+            public_id: item.public_id,
+            url: item.url,
+            secure_url: item.secure_url,
+          })),
+        },
+      },
+    });
+
     res.send("hi update product");
   } catch (err) {
     console.log(err);
@@ -57,6 +105,12 @@ exports.update = async (req, res) => {
 
 exports.remove = async (req, res) => {
   try {
+    const { id } = req.params;
+    await prisma.product.delete({
+      where: {
+        id: Number(id),
+      },
+    });
     res.send("remove product");
   } catch (err) {
     console.log(err);
@@ -66,7 +120,16 @@ exports.remove = async (req, res) => {
 
 exports.listby = async (req, res) => {
   try {
-    res.send("list by product");
+    const { sort, order, limit } = req.body;
+    console.log(sort, order, limit);
+    const products = await prisma.product.findMany({
+      take: limit,
+      orderBy: {
+        [sort]: order,
+      },
+      include: {category: true,},
+    });
+    res.send(products);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Server Error" });
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 // import controller
-const { create, list,update, remove, productby, searchfilter, listby } = require('../controllers/product')
+const { create, list,read,update, remove, listby, searchfilter } = require('../controllers/product')
 
 //@Endpoint : /product
 router.post('/product', create)
 router.get('/products/:count', list)
-router.put('/products/:id', update)
+router.get('/product/:id', read)
+router.put('/product/:id', update)
 router.delete('/product/:id', remove)
 router.post('/productby', listby)//ใช้sort สินค้า
 router.post('/search/filter',searchfilter)//ค้นหาสินค้า
