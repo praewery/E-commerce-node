81c5f8314443b43d1e6693418dda96c09e89fdeb
Praewa Choobanna <cytokinin8@gmail.com>
2025-01-22 19:34:18 +0700

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,68 @@
+const e = require("express")
+
 exports.listUsers = async(req, res) => {
     try{
         res.send("List Users")
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server Error"})
+    }
+}
+exports.changeStatus = async(req, res) => {
+    try{
+        res.send("chage status")
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server Error"})
+    }
+}
+exports.changeRole = async(req, res) => {
+    try{
+        res.send("change role")
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server Error"})
+    }
+}
+exports.getUserCart = async(req, res) => {
+    try{
+        res.send("usercart")
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server Error"})
+    }
+}
+exports.emptyCart = async(req, res) => {
+    try{
+        res.send("emptyCart")
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server Error"})
+    }
+}
+exports.saveAddress = async(req, res) => {
+    try{
+        res.send("saveAddress")
+
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({ message: "Server Error"})
+    }
+}
+exports.getOrder = async(req, res) => {
+    try{
+        res.send("saveAddress")
 
     }
     catch(err){
